b693ff91a7ec40632d28a910bf2de12af2213c9b
SaiMeghana10 <146646205+SaiMeghana10@users.noreply.github.com>
2023-10-08 00:48:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('.img-container img').forEach(image=>{
+    image.onclick = () =>{
+        document.querySelector('.popup-imag').style.display='block';
+        document.querySelector('.popup-imag img').src=image.getAttribute('src');
+
+    }
+    
+});
+document.querySelector('.popup-imag span').onclick = () =>{
+    document.querySelector('.popup-imag').style.display='none';
+
+}
